c1739084ebd559e8235675bdbc5076042cfab123
jhan-04 <gp098gp@gmail.com>
2022-07-16 21:40:25 +0900

    first commit

--- baekjoon/no. 1463.py
+++ baekjoon/no. 1463.py
@@ -3,15 +3,18 @@ N=int(sys.stdin.readline().strip())
 cnt=0
 case=[N]
 while 1:
+    if 1 in case: break
     cnt+=1
     test=[]
     for i in range(len(case)):
         a=case[i]
-        if a%3==0: 
+        if a%6==0: 
             test.append(a//3)
-        if a%2==0: 
+            test.append(a//2)
+        elif a%3==0: 
+            test.append(a//3)
+        elif a%2==0: 
             test.append(a//2)
         test.append(a-1)
     case=test
-    if 1 in test: break
 print(cnt)
---
+++ baekjoon/no.1920.py
@@ -0,0 +1,29 @@
+#못품 이진탐색 공부 필요
+import sys
+N=sys.stdin.readline()
+lst1=sorted(list(sys.stdin.readline().split()))
+M=int(sys.stdin.readline().strip())
+lst2=list(sys.stdin.readline().split())
+
+for x in lst2:
+    ind_max=len(lst1)-1
+    ind_min=0
+    cnt=0
+    while abs(ind_max-ind_min)>=1:
+        if abs(ind_max-ind_min)==1: 
+            if lst1[ind_max]==x or  lst1[ind_min]==x:
+                print(1)
+            else: print(0)
+            break
+        ind=(ind_max+ind_min)//2
+        if lst1[ind]==x: 
+            print(1)
+            break
+        elif lst1[ind]>x: ind_max=ind
+        else: ind_min=ind
+
+        
+        
+        
+
+#https://wayhome25.github.io/cs/2017/04/15/cs-16/
